d02bd0577ae6d0fc0d3291694fa42ae18fce7650
Antonio Trujillo <antrusant@gmail.com>
2022-02-13 16:46:39 +0100

    corrected L and D interchanged. Removed counter function, functionality moved to checkOrder() simler and more efficient now

--- RomanNumerals.py
+++ RomanNumerals.py
@@ -1,12 +1,28 @@
+romanPriority = {
+    "M"  : 1,
+    "CM" : 2,
+    "CD" : 2,
+    "D"  : 2,
+    "C"  : 3,
+    "XC" : 4,
+    "XL" : 4,   
+    "L"  : 4,
+    "X"  : 5,
+    "IX" : 6,
+    "V"  : 6,
+    "IV" : 6,
+    "I"  : 7
+}
+
 romanValues = {
     "M" : 1000,
     "CM": 900,
-    "L" : 500,
-    "CL": 400,
+    "D" : 500,
+    "CD": 400,
     "C" : 100,
     "XC": 90,
-    "D" : 50,
-    "XD": 40,
+    "L" : 50,
+    "XL": 40,
     "X" : 10,
     "IX": 9,
     "V" : 5,
@@ -17,34 +33,18 @@ romanValues = {
 arabValues = {
     1000 : "M",
     900  : "CM",
-    500  : "L",
-    400  : "CL",
+    500  : "D",
+    400  : "CD",
     100  : "C",
     90   : "XC",
-    50   : "D",
-    40   : "XD",
+    50   : "L",
+    40   : "XL",
     10   : "X",
     9    : "IX",
     5    : "V",
     4    : "IV",
     1    : "I"
 }
-romanPriority = {
-    "M"  : 1,
-    "CM" : 2,
-    "CL" : 3,
-    "L"  : 4,
-    "C"  : 5,
-    "XC" : 6,
-    "XD" : 7,   
-    "D"  : 8,
-    "X"  : 9,
-    "IX" : 10,
-    "V"  : 11,
-    "IV" : 12,
-    "I"  : 13
-}
-
 class RomanError(Exception):
     pass
 
@@ -55,71 +55,55 @@ def symbolExists(roman):
         if roman[i] not in romanPriority:
             raise RomanError("{} no es un símbolo romano".format(roman[i]))
 
-def countSymbols(roman):
-    symbolCounter= {
-    "M" : 0,
-    "CM": 0,
-    "L" : 0,
-    "CL": 0,
-    "C" : 0,
-    "XC": 0,
-    "XD": 0,
-    "D" : 0,
-    "X" : 0,
-    "IX": 0,
-    "V" : 0,
-    "IV": 0,
-    "I" : 0
-    }
-    i=0
-    if len(roman) == 1:
-            pass
-    while i<= (len(roman)-2):
+def currentAndNextSymbol(roman,i):
+    if i == len(roman)-1:
+        return (roman[-1], None)
+    elif i == (len(roman)-2):
         if (roman[i] + roman[i+1]) in romanPriority:
-            symbolCounter[roman[i] + roman[i+1]] +=1
-            i+=1
-            if i == (len(roman)-2):
-                symbolCounter[roman[-1]] += 1
-        elif roman[i] in romanPriority:
-            symbolCounter[roman[i]] +=1
-            if i == (len(roman)-2):
-                symbolCounter[roman[-1]] += 1
-        i+=1 
-    for symbol in symbolCounter:
-        if symbolCounter[symbol] > 1 and symbol in ("CM", "XC", "L", "V", "IV", "D", "IX", "CL", "XL"):
-            raise RomanError("{} no puede repetirse".format(symbol))
-        elif symbolCounter[symbol] > 3 and symbol in ("M", "C", "X", "I"):
-            raise RomanError("{} no puede repetirse más de 3 veces".format(symbol))
-    if  (symbolCounter["CM"] + symbolCounter["L"] + symbolCounter["CL"]) > 1 or \
-        (symbolCounter["XD"] + symbolCounter["D"] + symbolCounter["XC"]) > 1 or \
-        (symbolCounter["IX"] + symbolCounter["V"] + symbolCounter["IV"]) > 1 :
-            raise RomanError("Combinación no válida de 9/5/4")
-    if  ((symbolCounter["CM"] == 1 or symbolCounter["CL"] ==1) and symbolCounter["C"]) > 0 or \
-        ((symbolCounter["XC"] == 1 or symbolCounter["XD"] ==1) and symbolCounter["X"]) > 0 or \
-        ((symbolCounter["IX"] == 1 or symbolCounter["IV"] ==1) and symbolCounter["I"]) > 0 :
-            raise RomanError("Combinación no válida de 9/4/1")
-    
-def checkOrder(roman):
-    i=0
-    while i<= (len(roman)-2):
-        if len(roman) == 1:
-            pass
-        elif len(roman) == 2 and ((roman[i] + roman[i+1]) in romanPriority):
-            pass
-        elif  ((roman[i] + roman[i+1]) in romanPriority) and i == (len(roman)-2):
-            pass 
-        elif ((roman[i] + roman[i+1]) in romanPriority):
-            if romanPriority[roman[i] + roman[i+1]] >= romanPriority[roman[i+2]]:
-                raise RomanError("{} no puede ir antes de {}".format((roman[i] + roman[i+1]),roman[i+2]))
-            i+=1
-        elif (roman[i]) in romanPriority:
-            if romanPriority[roman[i]] > romanPriority[roman[i+1]]:
-                raise RomanError("{} no puede ir antes de {}".format(roman[i],roman[i+1]))
-        i+= 1        
+            return (roman[i:i+2], None)
+        else:
+            return (roman[i], roman[i+1])
+    elif i <= (len(roman) -3):
+        if (roman[i] + roman[i+1]) in romanPriority:
+            if i == (len(roman)-3):
+                return (roman[i:i+2], roman[i+2])
+            elif (roman[i+2] + roman[i+3]) in romanPriority:
+                return (roman[i:i+2], roman[i+2:i+4])
+            else: 
+                return (roman[i:i+2], roman[i+2])
+        elif (roman[i+1] + roman[i+2]) in romanPriority:
+            return (roman[i], roman[i+1:i+3])
+        else:
+            return (roman[i], roman[i+1])
 
+def checkOrder(roman):
+    i =0
+    repetitions = 0
+    while i< len(roman):
+        currentNext = currentAndNextSymbol(roman,i)
+        currentSymbol = currentNext[0]
+        nextSymbol = currentNext[1]
+        if nextSymbol == None:
+            return "ok"
+        elif romanPriority[currentSymbol] > romanPriority[nextSymbol]:
+            raise RomanError("{} no puede ir antes de {}".format(currentSymbol, nextSymbol))
+        elif (currentSymbol in ("CM", "CD", "D", "XL", "XC", "L", "IX", "V", "IV")) and romanPriority[currentSymbol] == romanPriority[nextSymbol]:
+            raise RomanError("{} no puede estar en un número romano que contenga además {}".format(currentSymbol, nextSymbol))
+        elif (currentSymbol in ("CM", "CD", "XL", "XC", "IX", "IV")) and romanPriority[currentSymbol] == (romanPriority[nextSymbol]-1):
+            raise RomanError("{} no puede ir seguido de {}".format(currentSymbol, nextSymbol))
+        elif currentSymbol in ("M", "C", "X", "I"):
+            if currentSymbol == nextSymbol:
+                repetitions +=1
+                if repetitions > 2:
+                    raise RomanError("{} no puede repetirse más de tres veces".format(currentSymbol))
+            else:
+                repetitions = 0
+        if currentSymbol in ("CM", "CD", "XC", "XL", "IX", "IV"):
+            i +=1
+        i +=1
+            
 def validateRoman(roman):
     symbolExists(roman)
-    countSymbols(roman)
     checkOrder(roman)
 
 def validateArab(arab):
@@ -134,17 +118,13 @@ def romanToArab(roman):
     validateRoman(roman)
     i = 0
     value = 0
-    if len(roman) == 1:
-            value += romanValues[roman[0]]
-            return value
-    while i <= (len(roman)-2):
-        if roman[i]+roman[i+1] in romanValues:
-            value += romanValues[roman[i]+roman[i+1]]
+    nextSymbol = 0 
+    while nextSymbol != None:
+        currentSymbol = currentAndNextSymbol(roman,i)[0]
+        nextSymbol = currentAndNextSymbol(roman,i)[1]
+        value += romanValues[currentSymbol]
+        if currentSymbol in ("CM", "CD", "XC", "XL", "IX", "IV"):
             i += 1
-        elif roman[i] in romanValues:
-            value += romanValues[roman[i]]
-        if i == (len(roman)-2):
-            value += romanValues[roman[-1]]
         i += 1
     return value
 
@@ -162,10 +142,8 @@ def arabToRoman(arab):
 
 if __name__ == '__main__':
 
-    for i in range(1,3999):
+    for i in range(1,4000):
         if romanToArab(arabToRoman(i)) != i:
             print("fallo en {}".format(i))
             print(romanToArab(arabToRoman(i)))
             print(arabToRoman(i))
-
-
